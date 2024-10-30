0ee41e995622709f246d37d67f899161c700ef50
vladditar <vlad@ditar.io>
2024-10-30 16:56:57 +0100

    Initial Commit

---
+++ expenses-tracker/expenses_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  expenses_trackerApp.swift
+//  expenses-tracker
+//
+//  Created by Vlad on 30/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct expenses_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
